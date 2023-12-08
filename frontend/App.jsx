ac5a40a496a3dab461e6d5e2a4c58b865ac8d365
Magaly Nicolas Sanchez <71893476+magaly1311@users.noreply.github.com>
2023-12-07 20:26:28 -0600

    backend

---
+++ frontend/App.jsx
@@ -0,0 +1,83 @@
+import React from "react"
+import logo from "./assets/dfinity.svg"
+/*
+ * Connect2ic provides essential utilities for IC app development
+ */
+import { createClient } from "@connect2ic/core"
+import { InternetIdentity } from "@connect2ic/core/providers/internet-identity"
+import { ConnectButton, ConnectDialog, Connect2ICProvider } from "@connect2ic/react"
+import "@connect2ic/core/style.css"
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+
+/*
+ * Import canister definitions like this:
+ */
+import * as cafeteria_magnate_backend from "../.dfx/local/canisters/cafeteria_magnate_backend"
+/*
+ * Some examples to get you started
+ */
+import { Transfer } from "./components/Transfer"
+import { Profile } from "./components/Profile"
+
+//import  Festividad  from "./components/Festividad"
+import  User  from "./components/FormModalUser"
+import  Productos  from "./components/FormModalProducto"
+import  Menu  from "./components/Menu"
+import  Home  from "./components/Principal"
+
+function App() {
+
+  return (
+    
+    <div className="App">
+      <BrowserRouter>
+      <Menu />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/usuario" element={<User/>} />
+        <Route path="/productos" element={<Eventos/>} />
+        <Route path="*" element={<Navigate to="/" replace />}/>
+      </Routes>
+      
+    </BrowserRouter>
+
+      <div className="auth-section">
+        <ConnectButton />
+      </div>
+      <ConnectDialog />
+
+      <p className="examples-title">
+        
+      </p>
+      <div className="examples">
+      
+      
+        
+
+      </div>
+
+    </div>
+  )
+}
+
+const client = createClient({
+  canisters: {
+    cafeteria_magnate_backend
+  },
+  providers: [
+    new InternetIdentity({ providerUrl: "http://127.0.0.1:8000/?canisterId=be2us-64aaa-aaaaa-qaabq-cai" })
+  ],
+  globalProviderConfig: {
+    /*
+     * Disables dev mode in production
+     * Should be enabled when using local canisters
+     */
+    dev: true,
+  },
+})
+
+export default () => (
+  <Connect2ICProvider client={client}>
+    <App />
+  </Connect2ICProvider>
+)
