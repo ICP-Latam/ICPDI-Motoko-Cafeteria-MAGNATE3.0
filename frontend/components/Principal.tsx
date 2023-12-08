444ca71dc3914e4a6350a9b84e483cc4a29c1c02
Magaly Nicolas Sanchez <71893476+magaly1311@users.noreply.github.com>
2023-12-07 20:57:03 -0600

    componentes

---
+++ frontend/components/Principal.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+
+// reactstrap components
+import { Container } from "reactstrap";
+//import Slider from "../Slider/Slider";
+
+function Home() {
+
+  return (
+    <div className="page-header header-filter" >
+      <Container >
+        <div className="content-center brand" style={{textAlign:'center'}}>
+          <h1 className="h1-seo">Cafeteria Magnate 3.0</h1>
+          <h3 className="d-none d-sm-block">
+            Una plataforma a tu alcance, fácil de manipular,
+            una gran variedad de productos en la Cafeteria.
+          </h3>
+        </div>
+        
+      </Container>
+      
+      
+      
+    </div>
+  );
+}
+
+export default Home;
