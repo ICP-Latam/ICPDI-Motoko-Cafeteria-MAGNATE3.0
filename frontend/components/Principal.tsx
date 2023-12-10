8e055103cb1db8615c5601912aa2f222f0b0e28c
Magaly Nicolas Sanchez <71893476+magaly1311@users.noreply.github.com>
2023-12-10 10:49:27 -0600

    logotipo

--- frontend/components/Principal.tsx
+++ frontend/components/Principal.tsx
@@ -13,9 +13,10 @@ function Home() {
         <div className="content-center brand" style={{textAlign:'center'}}>
           <h1 className="h1-seo">Cafeteria Magnate 3.0</h1>
           <h3 className="d-none d-sm-block">
-            Una plataforma que brinda a las personas un servicio de café así como 
-            servicio de comida y diferentes productos y por cada producto que adquiera 
-            dentro de esta cafetería.
+            Una plataforma que brinda poder proporcionarles a las personas un servicio de café así como 
+servicio de comida y diferentes productos y por cada producto que adquiera 
+dentro de esta cafetería puede obtener puntos e ir acumulándolos y canjearlos 
+ya desea por premios o por criptomonedas.
           </h3>
         </div>
         
