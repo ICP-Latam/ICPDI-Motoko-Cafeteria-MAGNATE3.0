9a51a00dfed18aa23631cb5ceda6abecf8cf5132
Magaly Nicolas Sanchez <71893476+magaly1311@users.noreply.github.com>
2023-12-10 10:50:39 -0600

    Logo

--- frontend/components/Principal.tsx
+++ frontend/components/Principal.tsx
@@ -1,29 +1,25 @@
 import React from "react";
-
-
-// reactstrap components
 import { Container } from "reactstrap";
-//import Slider from "../Slider/Slider";
+import cafeteriaImage from "./images/cafeteria.jpg"; // Ajusta la ruta según la ubicación real de la imagen
 
 function Home() {
-
   return (
-    <div className="page-header header-filter" >
-      <Container >
-        <div className="content-center brand" style={{textAlign:'center'}}>
+    <div className="page-header header-filter">
+      <Container>
+        <div className="content-center brand" style={{ textAlign: "center" }}>
           <h1 className="h1-seo">Cafeteria Magnate 3.0</h1>
           <h3 className="d-none d-sm-block">
-            Una plataforma que brinda poder proporcionarles a las personas un servicio de café así como 
-servicio de comida y diferentes productos y por cada producto que adquiera 
-dentro de esta cafetería puede obtener puntos e ir acumulándolos y canjearlos 
-ya desea por premios o por criptomonedas.
+            Una plataforma que brinda a las personas un servicio de café así como servicio de comida y diferentes productos y por cada producto que adquiera dentro de esta cafetería.
           </h3>
+
+          {/* Agregar la imagen */}
+          <img
+            src={cafeteriaImage}
+            alt="Cafeteria"
+            style={{ maxWidth: "100%", marginTop: "20px" }}
+          />
         </div>
-        
       </Container>
-      
-      
-      
     </div>
   );
 }
